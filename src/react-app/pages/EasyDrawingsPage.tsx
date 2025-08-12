5c686340b6596bfa52c461bbe837056b3121e2b1
Hrobex <artixiban@gmail.com>
2025-08-12 20:49:28 +0300

    Update EasyDrawingsPage.tsx

--- src/react-app/pages/EasyDrawingsPage.tsx
+++ src/react-app/pages/EasyDrawingsPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, ChangeEvent, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const faqData = [
